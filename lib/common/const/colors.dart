cb179cf394aed15750ac4f3aeb98db927d7046f6
JC <withj1123@gmail.com>
2022-07-11 11:22:47 +0900

    section 1 vid 1

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// 주색상
+const PRIMARY_COLOR = Color(0xFF22A45D);
+// 글자 색상
+const BODY_TEXT_COLOR = Color(0xFF868686);
+// 텍스트필드 배경 색상
+const INPUT_BG_COLOR = Color(0xFFFBFBFB);
+// 텍스트필드 테두리 색상
+const INPUT_BORDER_COLOR = Color(0xFFF3F2F2);
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    _App(),
+  );
+}
+
+class _App extends StatelessWidget {
+  const _App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
