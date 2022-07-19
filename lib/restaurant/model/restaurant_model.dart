19e8582fc0c4b0a4062ac1f09c3473a64a3ac9c9
JC <withj1123@gmail.com>
2022-07-19 15:11:59 +0900

    s3_v3

--- lib/restaurant/model/restaurant_model.dart
+++ lib/restaurant/model/restaurant_model.dart
@@ -1,3 +1,5 @@
+import 'package:actual/common/const/data.dart';
+
 enum RestaurantPriceRange {
   expensive,
   medium,
@@ -26,4 +28,22 @@ class RestaurantModel {
     required this.deliveryTime,
     required this.deliveryFee,
   });
+
+  factory RestaurantModel.fromJson({
+    required Map<String, dynamic> json,
+  }) {
+    return RestaurantModel(
+      id: json['id'],
+      name: json['name'],
+      thumbUrl: 'http://$ip${json['thumbUrl']}',
+      tags: List<String>.from(json['tags']),
+      priceRange: RestaurantPriceRange.values.firstWhere(
+            (e) => e.name == json['priceRange'],
+      ),
+      ratings: json['ratings'],
+      ratingsCount: json['ratingsCount'],
+      deliveryTime: json['deliveryTime'],
+      deliveryFee: json['deliveryFee'],
+    );
+  }
 }
--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -41,22 +41,8 @@ class RestaurantScreen extends StatelessWidget {
                   itemCount: snapshot.data!.length,
                   itemBuilder: (_, index) {
                     final item = snapshot.data![index];
-                    final pItem2 = RestaurantModel(
-                      item,
-                    );
-                    // parsed
-                    final pItem = RestaurantModel(
-                      id: item['id'],
-                      name: item['name'],
-                      thumbUrl: 'http://$ip${item['thumbUrl']}',
-                      tags: List<String>.from(item['tags']),
-                      priceRange: RestaurantPriceRange.values.firstWhere(
-                        (e) => e.name == item['priceRange'],
-                      ),
-                      ratings: item['ratings'],
-                      ratingsCount: item['ratingsCount'],
-                      deliveryTime: item['deliveryTime'],
-                      deliveryFee: item['deliveryFee'],
+                    final pItem = RestaurantModel.fromJson(
+                      json: item,
                     );
 
                     return RestaurantCard(
