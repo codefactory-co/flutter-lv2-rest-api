0d1974dd678e41ab2f959f6a634fd035193f5ce5
JC <withj1123@gmail.com>
2022-07-26 17:21:28 +0900

    s5_v6

--- lib/common/dio/dio.dart
+++ lib/common/dio/dio.dart
@@ -44,7 +44,15 @@ class CustomInterceptor extends Interceptor {
 
     return super.onRequest(options, handler);
   }
+
   // 2) 응답을 받을때
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    print('[RES] [${response.requestOptions.method}] ${response.requestOptions.uri}');
+
+    return super.onResponse(response, handler);
+  }
+
   // 3) 에러가 났을때
   @override
   void onError(DioError err, ErrorInterceptorHandler handler) async {
