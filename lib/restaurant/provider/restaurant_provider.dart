98c3e8f583751cb8fb167451ed42f37a18cd499c
JC <withj1123@gmail.com>
2022-07-28 10:10:32 +0900

    s7_v7

--- lib/restaurant/provider/restaurant_provider.dart
+++ lib/restaurant/provider/restaurant_provider.dart
@@ -23,7 +23,7 @@ class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
     paginate();
   }
 
-  paginate({
+  void paginate({
     int fetchCount = 20,
     // 추가로 데이터 더 가져오기
     // true - 추가로 데이터 더 가져옴
@@ -41,5 +41,17 @@ class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
     // 3) CursorPaginationError - 에러가 있는 상태
     // 4) CursorPaginationRefetching - 첫번째 페이지부터 다시 데이터를 가져올때
     // 5) CursorPaginationFetchMore - 추가 데이터를 paginate 해오라는 요청을 받았을때
+
+    // 바로 반환하는 상황
+    // 1) hasMore = false (기존 상태에서 이미 다음 데이터가 없다는 값을 들고있다면)
+    // 2) 로딩중 - fetchMore: true
+    //    fetchMore가 아닐때 - 새로고침의 의도가 있다
+    if(state is CursorPagination && !forceRefetch){
+      final pState = state as CursorPagination;
+
+      if(!pState.meta.hasMore){
+        return;
+      }
+    }
   }
 }
