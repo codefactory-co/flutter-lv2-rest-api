1a5919fc88b11911b8c88d26c849a845fdfd6fb6
JC <withj1123@gmail.com>
2022-08-01 22:05:02 +0900

    s15_v2

--- lib/order/component/order_card.dart
+++ lib/order/component/order_card.dart
@@ -43,11 +43,13 @@ class OrderCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Text(
             // 2022.09.01
             // 2022.9.1
             '${orderDate.year}.${orderDate.month.toString().padLeft(2, '0')}${orderDate.day.toString().padLeft(2, '0')} 주문완료'),
+        const SizedBox(height: 8.0),
         Row(
           children: [
             ClipRRect(
@@ -56,6 +58,7 @@ class OrderCard extends StatelessWidget {
             ),
             const SizedBox(width: 16.0),
             Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   name,
--- lib/order/view/order_screen.dart
+++ lib/order/view/order_screen.dart
@@ -1,3 +1,7 @@
+import 'package:actual/common/component/pagination_list_view.dart';
+import 'package:actual/order/component/order_card.dart';
+import 'package:actual/order/model/order_model.dart';
+import 'package:actual/order/provider/order_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,10 +10,11 @@ class OrderScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container(
-      child: Center(
-        child: Text('주문'),
-      ),
+    return PaginationListView<OrderModel>(
+      provider: orderProvider,
+      itemBuilder: <OrderModel>(_, index, model) {
+        return OrderCard.fromModel(model: model);
+      },
     );
   }
 }
